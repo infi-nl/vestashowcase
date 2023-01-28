9b2eb139d4da779c81818e556ea5b7b446578577
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-01-29 00:19:09 +0100

    Cheat on shuffler

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,12 @@ import { photos } from "./_components/photos";
 import { shuffle } from "../lib/util/shuffle";
 
 export function load() {
+  const head = photos[0];
+  const tail = photos.slice(1);
   return {
-    photos: shuffle(photos),
+    photos: [
+      head, // Force a specific picture with a real life person at the start.
+      ...shuffle(tail)
+    ],
   };
 }
