e9809b392e6accd790762e0fed26fdec86f3b5dc
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-01-28 15:02:29 +0100

    Add Tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../app.css";
+</script>
+<main class="p-4">
+  <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold text-3xl">Infi Vestaboard Showcase</h1>
+<p>Een showcase van wat we zoal met ons Vestaboard hebben gedaan.</p>
