0990e3617fc7482a9acdbf8f208d4f56d34c0774
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-01-28 23:14:52 +0100

    Complete website now

---
+++ src/routes/_components/Footer.svelte
@@ -0,0 +1,10 @@
+<footer class="
+  p-2 md:mx-4 border-b-0 text-center
+  shadow-lg shadow-black/50 border bg-gray-900 text-gray-50/50 opacity-95
+">
+  © 2023
+  |
+  <a class="hover:text-gray-50/80 hover:underline" href="https://infi.nl">infi.nl</a>
+  |
+  <a class="hover:text-gray-50/80 hover:underline" href="https://github.com/infi-nl/vestashowcase">GitHub Source</a>
+</footer>
