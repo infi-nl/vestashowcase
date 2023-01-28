7b8294baef655865516f461073a3fc3ad43c0639
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-01-28 15:43:13 +0100

    Initial index page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import "../app.css";
 </script>
-<main class="p-4">
+<main class="min-h-screen p-4 bg-gradient-to-b from-gray-50 to-gray-200 text-gray-700">
   <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1 class="font-bold text-3xl">Infi Vestaboard Showcase</h1>
-<p>Een showcase van wat we zoal met ons Vestaboard hebben gedaan.</p>
+<div class="shadow border bg-gray-50 p-4 max-w-[60rem] mx-auto">
+  <h1 class="text-center text-3xl font-bold">
+    Infi Vestaboard Showcase
+  </h1>
+  <p class="p-4 text-center">
+    Een fijn nerdy <a class="anchor" href="https://www.vestaboard.com/" target="_blank" rel="noreferrer">apparaat ↗</a> op kantoor om technische skills op los te laten?
+    Yes please!
+  </p>
+  <p class="text-center">
+    <a class="anchor" href="#about">Meer achtergrond lezen...</a>
+  </p>
+</div>
